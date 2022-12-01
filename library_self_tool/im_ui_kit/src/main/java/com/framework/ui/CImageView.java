789dd1369858a3cd5527ac86d72d945d78fb1d3f
aizp <18658251543@163.com>
2022-12-01 16:45:17 +0800

    add text image  view

---
+++ library_self_tool/im_ui_kit/src/main/java/com/framework/ui/CImageView.java
@@ -0,0 +1,14 @@
+package com.framework.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class CImageView extends AppCompatImageView {
+    public CImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ library_self_tool/im_ui_kit/src/main/java/com/framework/ui/CTextView.java
@@ -0,0 +1,14 @@
+package com.framework.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class CTextView extends AppCompatTextView {
+    public CTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
