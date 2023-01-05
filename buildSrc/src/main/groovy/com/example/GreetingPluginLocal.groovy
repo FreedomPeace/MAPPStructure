5b223aac5e2317ed077876a1ce9ece8a12697c04
aizp <18658251543@163.com>
2023-01-05 17:50:36 +0800

    plugin develop and publish

---
+++ buildSrc/src/main/groovy/com/example/GreetingPluginLocal.groovy
@@ -0,0 +1,12 @@
+package com.example
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GreetingPluginLocal implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        println("greeting hi $project.name")
+    }
+}
---
+++ plugin/src/main/java/com/example/plugin/GreetingPlugin.groovy
@@ -0,0 +1,15 @@
+package com.example.plugin
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GreetingPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.task("greeting"){
+            println("greeting hi $project.name")
+        }
+
+    }
+}
